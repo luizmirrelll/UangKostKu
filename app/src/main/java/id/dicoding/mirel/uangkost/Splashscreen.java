0964122cc020d0333039b1a56a12777e21efff03
luizmirrelll <aloysius.mirel96@gmail.com>
2020-11-11 23:17:18 +0700

    UangKost

---
+++ app/src/main/java/id/dicoding/mirel/uangkost/Splashscreen.java
@@ -0,0 +1,16 @@
+package id.dicoding.mirel.uangkost;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
